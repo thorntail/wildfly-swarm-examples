575417501abb03381aa34c5a055d89b7f546f376
Jeff Mesnil <jmesnil@gmail.com>
2015-10-13 12:40:19 +0200

    fix messaging example
    * use JMS 2.0 API and lambda to remove boilerplate code irrelevant to
      the example
    * add deps to wildfly-swarm-jca (not sure if that one shouldn't be a
      dep of wildfly-swarm-messaging instead but it is required to create
      the default pooled-connection-factory)

--- messaging/src/main/java/org/wildfly/swarm/examples/messaging/MyService.java
+++ messaging/src/main/java/org/wildfly/swarm/examples/messaging/MyService.java
@@ -1,13 +1,10 @@
 package org.wildfly.swarm.examples.messaging;
 
-import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
 import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
-import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -24,7 +21,7 @@ public class MyService implements Service<Void> {
 
     private final String destinationName;
 
-    private Connection conn;
+    private JMSContext context;
 
     public MyService(String destinationName) {
         this.destinationName = destinationName;
@@ -34,35 +31,27 @@ public class MyService implements Service<Void> {
         try {
             Context ctx = new InitialContext();
             ConnectionFactory factory = (ConnectionFactory) ctx.lookup("ConnectionFactory");
-            this.conn = factory.createConnection();
 
+            this.context = factory.createContext();
             Destination destination = (Destination) ctx.lookup(destinationName);
-            Session sess = conn.createSession();
-            MessageConsumer consumer = sess.createConsumer(destination);
-            startContext.complete();
+
+            JMSConsumer consumer = context.createConsumer(destination);
             System.err.println("Starting to receive from " + destination);
-            consumer.setMessageListener(new MessageListener() {
-                public void onMessage(Message message) {
-                    try {
-                        System.err.println("received: " + ((TextMessage) message).getText());
-                    } catch (JMSException e) {
-                        e.printStackTrace();
-                    }
+            consumer.setMessageListener( message -> {
+                try {
+                    System.err.println("received: " + ((TextMessage) message).getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
                 }
             });
-            conn.start();
+            startContext.complete();
         } catch (Throwable t) {
             throw new StartException(t);
         }
     }
 
     public void stop(StopContext stopContext) {
-        try {
-            this.conn.close();
-        } catch (JMSException e) {
-            e.printStackTrace();
-        }
-
+        this.context.close();
     }
 
     public Void getValue() throws IllegalStateException, IllegalArgumentException {
